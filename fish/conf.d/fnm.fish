e8f6c254989874ce2dfaf1663ba2d58dcf1cf631
Myles Berueda <myles.berueda@codemithril.com>
2021-09-28 14:06:51 -1000

    added comments for fnm

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -1,1 +1,11 @@
+# To use fnm as the node version manager, first install it:
+# curl -fsSL https://fnm.vercel.app/install | bash
+#
+# Then, set up the fnm path:
+# set -U fish_user_paths ~/.fnm $fish_user_paths
+#
+# Finally, add shell completions
+# fnm completions --shell fish
+#
+# References: https://github.com/Schniz/fnm
 fnm env | source
