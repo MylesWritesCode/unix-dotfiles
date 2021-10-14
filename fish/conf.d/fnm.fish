f07b71f69b14ba25c121fee8a42ebf0512e41ff0
misfit <misfit@misfit.com>
2021-10-14 08:14:34 -0400

    honestly don't know
    The only thing I changed was the gaps in i3, but all this crap is going
    up with this commit now.

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -1,4 +1,3 @@
-# Instructions
 # To use fnm as the node version manager, first install it:
 # curl -fsSL https://fnm.vercel.app/install | bash
 #
@@ -8,8 +7,7 @@
 # Finally, add shell completions
 # fnm completions --shell fish
 #
-# References
-# https://github.com/Schniz/fnm
+# References: https://github.com/Schniz/fnm
 
 if test -d ~/.fnm
   fnm env | source
