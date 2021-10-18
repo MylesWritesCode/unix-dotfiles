36d3154b6fdebb7d81b5cbc9a06b613faaa482e5
Myles Berueda <myles.berueda@codemithril.com>
2021-10-17 16:31:29 -1000

    removed right side clock from fish
    The clock is now displayed by OhMyPosh.

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
 end
 
+function fish_right_prompt
+  # Intentionally left blank; This is now being taken care of by oh-my-posh.
+end
+
 # Commands that will always be run, regardless of interactivity
 oh-my-posh --init --shell fish --config ~/.config/windows-terminal/writescode.json | source
 set -g theme_nerd_fonts yes
